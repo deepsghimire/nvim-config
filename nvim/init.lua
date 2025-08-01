5b3c74630d559b311ad0239f581dd556e719aea6
Sandeep Ghimire <deepsghimire1@gmail.com>
2025-08-01 20:08:23 +0545

    Revert "migrate most of the config to fennel"
    This reverts commit 466e485876a317112b9dc7401a8b229df0d52428.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,39 +1,11 @@
-local function bootstrap(url, ref)
-	local name = url:gsub(".*/", "")
-	local path = vim.fn.stdpath([[data]]) .. "/lazy/" .. name
+local options = require("options")
+local keymaps = require("keymaps")
+local plugman = require("plugman")
 
-	if vim.fn.isdirectory(path) == 0 then
-		print(name .. ": installing in data dir...")
-
-		vim.fn.system({ "git", "clone", url, path })
-		if ref then
-			vim.fn.system({ "git", "-C", path, "checkout", ref })
-		end
-
-		vim.cmd([[redraw]])
-		print(name .. ": finished installing")
-	end
-	vim.opt.runtimepath:prepend(path)
+local function configure()
+	options:setup()
+	keymaps:setup()
+	plugman:setup()
 end
 
-bootstrap("https://github.com/udayvir-singh/tangerine.nvim")
-
--- Optional and only needed if you also want the macros
-bootstrap("https://github.com/udayvir-singh/hibiscus.nvim")
-
-require("tangerine").setup({
-	target = vim.fn.stdpath([[data]]) .. "/tangerine",
-
-	-- compile files in &rtp
-	rtpdirs = {
-		"ftplugin",
-	},
-
-	compiler = {
-		-- disable popup showing compiled files
-		verbose = false,
-
-		-- compile every time changes are made to fennel files or on entering vim
-		hooks = { "onsave", "oninit" },
-	},
-})
+configure()
