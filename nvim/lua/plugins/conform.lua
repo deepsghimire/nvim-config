cc267872fbda6f9eb8e76a1e7b4f0f04a66dfcc1
Sandeep Ghimire <sandeep@teslatech.com.np>
2026-05-09 15:16:23 +0545

    change formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,13 +1,13 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "isort", "black" },
-		},
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-	},
+    "stevearc/conform.nvim",
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
+        },
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_fallback = true,
+        },
+    },
 }
