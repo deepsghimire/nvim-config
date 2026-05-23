b24f262ace173bed6d7b0b44ea6b73f6079376cc
Sandeep Ghimire <sandeep@teslatech.com.np>
2026-05-23 21:36:39 +0545

    add signature hint automatically

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -42,6 +42,8 @@ return {
             default = { 'lsp', 'path', 'snippets', 'buffer' },
         },
 
+        signature = { enabled = true, window = { border = 'rounded' } },
+
         -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
         -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
         -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
