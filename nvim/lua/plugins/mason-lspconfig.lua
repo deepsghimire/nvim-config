2048cb026655e14393e85d429b33697bcc824e35
Sandeep Ghimire <sandeep@teslatech.com.np>
2026-05-23 22:27:47 +0545

    refactor: simplify LSP setup and integrate blink.cmp capabilities

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,11 +1,7 @@
 return {
     "williamboman/mason-lspconfig.nvim",
-    dependencies = { "mason.nvim", "hrsh7th/cmp-nvim-lsp" },
-    config = function()
-        local masonconfig = require("mason-lspconfig")
-
-        masonconfig.setup({
-            ensure_installed = { "pyright", "lua_ls", "rust_analyzer", "bashls" },
-        })
-    end,
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+        ensure_installed = { "pyright", "lua_ls", "rust_analyzer", "bashls", "gopls" },
+    },
 }
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,23 @@
 return {
-	"neovim/nvim-lspconfig",
+    "neovim/nvim-lspconfig",
+    dependencies = { "saghen/blink.cmp" },
+    config = function()
+        local lspconfig = require("lspconfig")
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+        -- Minimalist auto-setup for servers installed via Mason
+        require("mason-lspconfig").setup_handlers({
+            function(server_name)
+                lspconfig[server_name].setup({
+                    capabilities = capabilities,
+                })
+            end,
+            -- Dedicated config for Lua to handle Neovim APIs (provided by lazydev)
+            ["lua_ls"] = function()
+                lspconfig.lua_ls.setup({
+                    capabilities = capabilities,
+                })
+            end,
+        })
+    end,
 }
