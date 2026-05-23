161ed32aec55537009f2d7992d4c6dd8fda0ca01
Sandeep Ghimire <sandeep@teslatech.com.np>
2026-05-23 22:41:07 +0545

    chore: cleanup temporary files and finalize lsp/snippet configuration

--- nvim/lua/plugins/mason-lspconfig.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    opts = {
-        ensure_installed = { "pyright", "lua_ls", "rust_analyzer", "bashls", "gopls" },
-    },
-}
