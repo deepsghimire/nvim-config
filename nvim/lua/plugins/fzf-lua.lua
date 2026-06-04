88f3762f7b4781f9f3046f9944e02a82f20fa59e
Sandeep Ghimire <sandeep@teslatech.com.np>
2026-06-04 09:00:27 +0545

    keymap for fzf-lua

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -4,8 +4,21 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         -- calling `setup` is optional for customization
-        require("fzf-lua").setup({
-            "fzf-native",
+        local fzf = require("fzf-lua")
+        fzf.setup({
+            -- 1. Global defaults for ALL finders (files, grep, buffers, etc.)
+            defaults = {
+                actions = {
+                    -- Forces ctrl-g to dump EVERYTHING into quickfix
+                    ["ctrl-q"] = fzf.actions.file_to_qf,
+                },
+            },
+            -- 2. Force the fzf binary interface to accept it globally
+            keymap = {
+                fzf = {
+                    ["ctrl-q"] = "select-all+accept",
+                },
+            },
         })
     end,
 }
